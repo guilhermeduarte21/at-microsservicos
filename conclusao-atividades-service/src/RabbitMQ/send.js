423245b6fcaf12ffc71914c0817308e8da64f2a0
Duarte <guilhermeduartt21@gmail.com>
2021-04-09 15:28:58 -0300

    feat: RabbitMP send

---
+++ conclusao-atividades-service/src/RabbitMQ/send.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const amqp = require("amqplib/callback_api");
+
+function enviar(msg) {
+  amqp.connect(process.env.AMQP_URL, function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+
+      var queue = "hello";
+
+      channel.assertQueue(queue, {
+        durable: false,
+      });
+      channel.sendToQueue(queue, Buffer.from(msg));
+
+      console.log(" [x] Sent %s", msg);
+    });
+  });
+}
+
+module.exports = {
+  enviar,
+};
